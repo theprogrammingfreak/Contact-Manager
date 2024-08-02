39ad87e46c564bab6803127276cd9f37a31a4014
Satwik <satwik0655@gmail.com>
2024-08-02 21:41:41 +0530

    Added validation of various fields for the signup form

--- src/main/java/com/scm20/controllers/PageController.java
+++ src/main/java/com/scm20/controllers/PageController.java
@@ -3,6 +3,7 @@ package com.scm20.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import com.scm20.helpers.MessageType;
 import com.scm20.services.UserService;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 
 ;
 
@@ -72,13 +74,17 @@ public class PageController {
     //processing register 
     
     @RequestMapping(value ="/do-register", method = RequestMethod.POST)
-    public String processRegister(@ModelAttribute UserForm userForm,HttpSession session){
+    public String processRegister(@Valid @ModelAttribute UserForm userForm,BindingResult rBindingResult, HttpSession session){
         System.out.println("Processing registration");
         // fetch form data
         // UserForm (class to fetch all the data from)
         System.out.print(userForm);
         
         // validate form data
+        if(rBindingResult.hasErrors()){
+            return "register";
+        }
+
         // save to database
 
         // userservices
--- src/main/java/com/scm20/forms/UserForm.java
+++ src/main/java/com/scm20/forms/UserForm.java
@@ -1,5 +1,8 @@
 package com.scm20.forms;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,10 +18,22 @@ import lombok.ToString;
 @ToString
 public class UserForm {
 
+    @NotBlank(message = "Username is required")
+    @Size(min = 3,message = "Min 3 Characters are required")
     private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid Email Address")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6,message = "Min 6 characters are required")
     private String password;
+
+    @NotBlank(message = "About is required")
     private String about;
+    
+    @Size(min = 8,max = 12,message = "Invalid Phone Number")
     private String phoneNumber;
 
 }
